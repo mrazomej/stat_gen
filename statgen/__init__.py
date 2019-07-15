c3130918b8f8a775052c98e724468cb56e2a23a3
mrazomej <manuel.razo.m@gmail.com>
2019-07-15 22:36:50 +0200

    starting utils for project

---
+++ statgen/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top level package for statistical genetics notes utilities"""
+
+
+# from . import mcmc
+from . import viz
+
+__author__ = """Manue Razo-Mejia"""
+__email__ = """mrazomej@caltech.edu"""
+__version__ = '0.0.1'
