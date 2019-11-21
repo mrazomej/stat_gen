5c3312d18cedf68136ad11ca489a6ceb43d912e5
mrazomej <manuel.razo.m@gmail.com>
2019-11-21 12:24:29 -0800

    got script to modfiy tex files

--- fig/rename_figs.py
+++ fig/rename_figs.py
@@ -4,6 +4,7 @@
 import os
 import glob
 import pandas as pd
+import re
 
 # %%
 
@@ -15,6 +16,7 @@ chapters = xl.sheet_names
 
 # %%
 
+print('Updating Figures and Scripts')
 # Loop through chapters
 for chap in chapters:
     # Read figures in chapter
@@ -51,4 +53,44 @@ for chap in chapters:
                 print(f'{q} -> {new_script}')
                 os.rename(q, new_script)
 
+print('Done!')
+
+# %%
+
+print('Updating latex files')
+# Loop through chapters
+for chap in chapters:
+    print(f'chapter: {chap}')
+    # Locate tex files
+    tex_files = glob.glob(f'../doc/chapters/{chap}/*tex')
+    # read figures
+    figs = xl.parse(chap)
+    # Loop through tex files
+    for tex_file in tex_files:
+        print('reading:')
+        print(tex_file)
+        # Read lines into memory
+        with open(tex_file, 'r') as tex:
+            lines = tex.readlines()
+        # Loop through figures
+        for f_idx, f in figs.iterrows():
+            # Define figure name 
+            f_name = f'{f.chapter:02d}_{f.section:02d}_' + \
+                     f'{f.number:02d}_{f.fig_name}'
+            # Define pattern
+            pattern = re.compile("{(.*)" +
+                                f"{chap}(.*){f.fig_name}" + 
+                                "(.*)}")
+            # Loop through lines
+            for i, line in enumerate(lines):
+                match = re.findall(pattern, line)
+                if len(match) > 0:
+                    print(f.fig_name)
+                    print(f'Found on line {i+1}')
+                    lines[i] = '  {../fig/' + \
+                    f'{chap}/{f_name}{match[-1][-1]}' + '}\n'
+                    print(lines[i])
+        # and write everything back
+        with open(tex_file, 'w') as tex:
+            tex.writelines(lines)
 # %%
